56cc75d9cfea26b039026b7a07b8d63ca7590831
Abu Raihan <aburaihan8721@gmail.comm>
2021-09-26 18:44:54 +0600

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="container header">
+            <h1>Select Your Desired Actors</h1>
+            <h1>Budget : 300 Cores</h1>
+        </div>
+    )
+}
